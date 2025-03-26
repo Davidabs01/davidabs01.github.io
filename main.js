1aee56e95d216b972be7824f54cca48e12b4535d
Davidabs01 <bamigboyedavidalonge2998@gmail.com>
2025-03-26 11:37:37 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "chrome://branding/content/about-logo@2x.png") {
+    myImage.setAttribute("src", "image/www.jpg");
+  } else {
+    myImage.setAttribute("src", "chrome://branding/content/about-logo@2x.png");
+  }
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+
+  myButton.addEventListener("click", () => {
+    setUserName();
+  });
+   
